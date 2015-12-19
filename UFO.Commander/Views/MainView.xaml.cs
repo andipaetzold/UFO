4f5bf1ba243a2f8f0e78d5a5e6c034595c0a8b5b
Andreas Pätzold <andi.paetzold@gmail.com>
2015-12-19 22:43:15 +0100

    Day Program is colored by categories

--- UFO.Commander/Views/MainView.xaml.cs
+++ UFO.Commander/Views/MainView.xaml.cs
@@ -5,6 +5,7 @@
     using System.Windows;
     using System.Windows.Controls;
     using System.Windows.Data;
+    using System.Windows.Media;
     using UFO.Commander.Util;
     using UFO.Commander.ViewModels;
     using UFO.Domain;
@@ -39,11 +40,54 @@
                         Header = header,
                         SelectedItemBinding = binding,
                         DisplayMemberPath = nameof(Artist.Name),
-                        ItemsSource = ((MainViewModel)DataContext).ArtistsWithNull
+                        ItemsSource = ((MainViewModel)DataContext).ArtistsWithNull,
+                        CellStyle =
+                            new Style
+                                {
+                                    Setters =
+                                        {
+                                            new Setter
+                                                {
+                                                    Property = BackgroundProperty,
+                                                    Value =
+                                                        new Binding(
+                                                            $"{nameof(VenueProgram.Times)}[{hour}].{nameof(Artist)}.{nameof(Category)}")
+                                                            {
+                                                                Converter = new CategoryToBrushConverter()
+                                                            }
+                                                }
+                                        }
+                                }
                     };
 
                 DayProgram.Columns.Add(column);
             }
         }
+
+        #region Nested type: CategoryToBrushConverter
+
+        private class CategoryToBrushConverter : IValueConverter
+        {
+            #region IValueConverter Members
+
+            public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+            {
+                if (value == null)
+                {
+                    return new SolidColorBrush(Color.FromRgb(255, 255, 255));
+                }
+                var bytes = BitConverter.GetBytes(value.GetHashCode());
+                return new SolidColorBrush(Color.FromRgb(bytes[0], bytes[1], bytes[2]));
+            }
+
+            public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            {
+                throw new NotImplementedException();
+            }
+
+            #endregion
+        }
+
+        #endregion
     }
 }
