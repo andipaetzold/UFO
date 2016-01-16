9b0fd8d9f150623295e8258df861df7c2a5cb921
Andreas Pätzold <andi.paetzold@gmail.com>
2016-01-16 20:55:04 +0100

    Added error page

---
+++ UFO.Web/src/util/WebServiceAvailableCheck.java
@@ -0,0 +1,30 @@
+package util;
+
+import services.UltimateFestivalOrganizer;
+
+import javax.faces.context.FacesContext;
+import javax.faces.event.PhaseEvent;
+import javax.faces.event.PhaseId;
+import javax.faces.event.PhaseListener;
+
+public class WebServiceAvailableCheck implements PhaseListener {
+    @Override
+    public void afterPhase(PhaseEvent phaseEvent) {
+    }
+
+    @Override
+    public void beforePhase(PhaseEvent phaseEvent) {
+        try {
+            new UltimateFestivalOrganizer();
+        }
+        catch (Exception e) {
+            FacesContext facesContext = FacesContext.getCurrentInstance();
+            facesContext.getApplication().getNavigationHandler().handleNavigation(facesContext, null, "error");
+        }
+    }
+
+    @Override
+    public PhaseId getPhaseId() {
+        return PhaseId.RESTORE_VIEW;
+    }
+}
