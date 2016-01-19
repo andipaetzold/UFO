d82a08dbd84e179baf35cd0b48ca49a90ef1ac7f
Andreas Pätzold <andi.paetzold@gmail.com>
2016-01-20 00:20:54 +0100

    Implemented Venues Page

---
+++ UFO.Web/src/beans/VenuesBean.java
@@ -0,0 +1,40 @@
+package beans;
+
+import services.UltimateFestivalOrganizer;
+import services.UltimateFestivalOrganizerSoap;
+import services.Venue;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+import java.util.List;
+import java.util.Map;
+
+@ManagedBean(name = "venuesBean")
+@RequestScoped
+public class VenuesBean {
+    private Venue venue;
+
+    public List<Venue> getAll() {
+        UltimateFestivalOrganizer service = new UltimateFestivalOrganizer();
+        UltimateFestivalOrganizerSoap ufo = service.getUltimateFestivalOrganizerSoap();
+
+        return ufo.getAllVenues().getVenue();
+    }
+
+    public void updateDialog() {
+        Map<String, String> requestParams = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
+        if (!requestParams.containsKey("id")) {
+            return;
+        }
+        int id = Integer.valueOf(requestParams.get("id"));
+
+        UltimateFestivalOrganizer service = new UltimateFestivalOrganizer();
+        UltimateFestivalOrganizerSoap ufo = service.getUltimateFestivalOrganizerSoap();
+        venue = ufo.getVenueById(id);
+    }
+
+    public Venue getVenue() {
+        return venue;
+    }
+}
