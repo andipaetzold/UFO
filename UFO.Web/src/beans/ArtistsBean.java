cdcce3a65a3043a375319b86a821ba828fed21ab
Andreas Pätzold <andi.paetzold@gmail.com>
2016-01-19 23:06:51 +0100

    artist is shown in a dialog

--- UFO.Web/src/beans/ArtistsBean.java
+++ UFO.Web/src/beans/ArtistsBean.java
@@ -1,20 +1,43 @@
 package beans;
 
+import org.primefaces.context.RequestContext;
 import services.Artist;
 import services.UltimateFestivalOrganizer;
 import services.UltimateFestivalOrganizerSoap;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @ManagedBean(name = "artistsBean")
 @RequestScoped
 public class ArtistsBean {
+    private Artist artist;
+
     public List<Artist> getAll() {
         UltimateFestivalOrganizer service = new UltimateFestivalOrganizer();
         UltimateFestivalOrganizerSoap ufo = service.getUltimateFestivalOrganizerSoap();
 
         return ufo.getAllButDeletedArtists().getArtist();
+    }
+
+    public void updateDialog() {
+        Map<String, String> requestParams = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap();
+        if (!requestParams.containsKey("id")) {
+            return;
+        }
+        int id = Integer.valueOf(requestParams.get("id"));
+
+        UltimateFestivalOrganizer service = new UltimateFestivalOrganizer();
+        UltimateFestivalOrganizerSoap ufo = service.getUltimateFestivalOrganizerSoap();
+        artist = ufo.getArtistById(id);
+    }
+
+    public Artist getArtist() {
+        return artist;
     }
 }
